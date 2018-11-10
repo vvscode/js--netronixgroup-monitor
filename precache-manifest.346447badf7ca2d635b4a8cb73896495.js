d8835e8c624aa641b1fef019af509da2313ef6cf
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-11-10 15:22:18 +0400

    Updates

---
+++ precache-manifest.346447badf7ca2d635b4a8cb73896495.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b68d28a9f8c25e6b4991",
+    "url": "/js--netronixgroup-monitor/static/js/runtime~main.b68d28a9.js"
+  },
+  {
+    "revision": "0d57b6dafd9e08041f22",
+    "url": "/js--netronixgroup-monitor/static/js/main.0d57b6da.chunk.js"
+  },
+  {
+    "revision": "562507a0d3669831317e",
+    "url": "/js--netronixgroup-monitor/static/js/1.562507a0.chunk.js"
+  },
+  {
+    "revision": "0d57b6dafd9e08041f22",
+    "url": "/js--netronixgroup-monitor/static/css/main.e76a27b4.chunk.css"
+  },
+  {
+    "revision": "e77c4f4deefd49fcbad7c4479f050465",
+    "url": "/js--netronixgroup-monitor/index.html"
+  }
+];
